463e7fb29a2d2930edd59ec2dff42d701afe8a17
JamieFernsQA <JamieFerns@academytrainee.com>
2018-04-20 15:43:23 +0100

    test submission

--- assessment.js
+++ assessment.js
@@ -1,20 +1,58 @@
 
-function isPrime(number){
+function  isPrime(){
+    if (PrimeCandidate.value == 1){ 
+        console.log("1 is a prime number");
+            }
+else if  ((PrimeCandidate.value % !1 == 0)  && (PrimeCandidate.value % !(PrimeCandidate.value == 0))) {
+    console.log("it is not a prime number");
+}
 
+else { console.log("it is a prime number")
+}
 }
   
-function amountToCoins(amount, coins){
+function amountToCoins(){
+
+    coinage = ConvertToCoins.value;
+    AvailableCoins = [50, 20, 10, 5, 2, 1];
+    CoinOutput = (coinage / AvailableCoins);
+
+    console.log(CoinOutput);;
+}
+
+function uniqueLetters(){
+ document.getElementById("UniqueLetters");
+ 
+
+    console.log("");
 
 }
 
-function uniqueLetters(string){
+
+
+function factorial(){
+
+ for(x = NumToFactorial.value; x > 0; --x){
+    y = x * x; 
+    console.log(y);}
+
+    
 
 }
 
-function factorial(number){
+function fibonacci(){
 
+    for (FiboNum = 1; FiboNum < 1000; FiboNum += FiboNum )
+         
+
+        console.log(FiboNum);
+    
+
+    
 }
 
-function fibonacci(number){
 
-}
+
+
+
+
